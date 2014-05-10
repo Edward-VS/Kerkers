d902c06b5cce41e53c5c81f255ff788d5371ff6c
ChristofVermeersch <christof.vermeersch@live.be>
2014-05-10 21:35:12 +0200

    Update

---
+++ Examenproject/src/dungeons/obstacle/DoorTest.java
@@ -0,0 +1,64 @@
+/**
+ * 
+ */
+package dungeons.obstacle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Christof
+ *
+ */
+public class DoorTest {
+
+	/**
+	 * Test method for {@link dungeons.obstacle.Door#canBeAnObstacleAt(dungeons.Square, dungeons.util.Direction)}.
+	 */
+	@Test
+	public void testCanBeAnObstacleAt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link dungeons.obstacle.Door#Door()}.
+	 */
+	@Test
+	public void testDoor() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link dungeons.obstacle.Door#Door(java.lang.Boolean)}.
+	 */
+	@Test
+	public void testDoorBoolean() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link dungeons.obstacle.Door#isOpen()}.
+	 */
+	@Test
+	public void testIsOpen() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link dungeons.obstacle.Door#setOpen(java.lang.Boolean)}.
+	 */
+	@Test
+	public void testSetOpenBoolean() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link dungeons.obstacle.Door#setOpen()}.
+	 */
+	@Test
+	public void testSetOpen() {
+		fail("Not yet implemented");
+	}
+
+}
